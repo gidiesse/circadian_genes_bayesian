7549454a16108bcdb706181312f218dc003b9e34
Aledig10 <alessandroangelo.digianni@gmail.com>
2024-12-02 12:31:11 +0100

    Add files via upload

---
+++ UPDATES.h
@@ -0,0 +1,15 @@
+//
+// Created by Alessandro on 12/2/2024.
+//
+
+#ifndef BAYESIAN_STATISTICS_PROJECT_UPDATES_H
+#define BAYESIAN_STATISTICS_PROJECT_UPDATES_H
+
+
+class UPDATES {
+private:
+public:
+};
+
+
+#endif //BAYESIAN_STATISTICS_PROJECT_UPDATES_H
