240ddb9d046b81acc848cafd9f35daca2ef9fb5e
Aledig10 <alessandroangelo.digianni@gmail.com>
2024-12-09 15:11:15 +0100

    Add files via upload

--- MCMC.cpp
+++ MCMC.cpp
@@ -1,239 +1,201 @@
 #include <iostream>
-#include <vector>
-#include <cmath>
-#include <random>
-#include <fstream>
-#include <sstream>
 #include <armadillo>
 
-using namespace std;
-using namespace arma;
+void load_matrix (const std::string& file_path, const std::string& file_name, arma::mat &mat, bool print);
+void load_vector (const std::string& file_path, const std::string& file_name, arma::vec &vec, bool print);
+void load_constant (const std::string& file_path, const std::string& file_name, int num, bool print);
+arma::mat chol_cov (const arma::mat& mat);
+int main()
+{
+    std::string file_path = "C:/Users/aless/Downloads/";
+    arma::arma_rng::set_seed(230518);
 
-int main(){
-    mt19937 generator(500);
-    std::ifstream Yfile("Y.csv");
-    std::string line;
-    std::vector<std::vector<double>> Y;
+    arma::mat Y;
+    load_matrix(file_path, "Y.csv", Y, false);
 
-    while (std::getline(Yfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
+    int p = Y.n_cols;
+    int T = Y.n_rows;
 
-        // Leggi i valori separati da virgola
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
-        }
-        Y.push_back(row); // Aggiungi la riga alla matrice
-    }
-    Yfile.close();
-    std::ifstream tijfile("tij.csv");
-    std::vector<double> tij;
+    const int q = 5;
 
-    while (std::getline(tijfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        tij.push_back(std::stod(value)); // Converte la stringa in double
-    }
-    tijfile.close();
+    arma::vec t_ij = arma::linspace<arma::vec>(0, 46, 24);
+    t_ij = t_ij / arma::max(t_ij);    // standardized time points
 
-    std::vector<double> tg;
-    std::ifstream tgfile("tg.csv");
-    while (std::getline(tgfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        tg.push_back(std::stod(value)); // Converte la stringa in double
-    }
-    tgfile.close();
-    int p;
-    std::ifstream pfile("p.csv");
-    while (std::getline(pfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        p=std::stod(value); // Converte la stringa in double
-    }
-    pfile.close();
-    int T;
-    std::ifstream Tfile("T.csv");
-    while (std::getline(Tfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        T=std::stod(value); // Converte la stringa in double
-    }
-    Tfile.close();
-    std::vector<std::vector<double>> B;
-    std::ifstream Bfile("B.csv");
-    while (std::getline(Bfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
-        }
-        B.push_back(row); // Aggiungi la riga alla matrice
-    }
-    Bfile.close();
-    std::vector<std::vector<double>> eta;
-    std::ifstream etafile("eta.csv");
-    while (std::getline(etafile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
-        }
-        eta.push_back(row); // Aggiungi la riga alla matrice
-    }
-    etafile.close();
-    std::vector<double> lambda;
-    std::ifstream Lambdafile("Lambda.csv");
+    arma::vec tg = arma::linspace<arma::vec>(0, 46, 461) / 46;
 
-    if (std::getline(Lambdafile, line)) {
-        std::stringstream ss(line); // Usa stringstream per analizzare la riga
-        std::string value;
-        while (std::getline(ss, value, ',')) {
-            lambda.push_back(std::stoi(value)); // Converti la stringa in int e aggiungi al vettore
-        }
+    arma::mat B(T, 2*q, arma::fill::zeros);
+    arma::mat B_pred(tg.n_elem, 2*q, arma::fill::zeros);
+
+    arma::rowvec initial_periods = {8, 12, 16, 24, 48};
+    arma::rowvec periods = initial_periods / 2;
+    arma::rowvec full_periods = periods / 46;
+
+    for (size_t h = 0; h < full_periods.n_elem; ++h)
+    {
+        B.col(2*h) = sin(((2*arma::datum::pi) / (full_periods(h)))*t_ij);
+        B_pred.col(2*h) = sin(((2*arma::datum::pi) / (full_periods(h)))*tg);
+
+        B.col(2*h+1) = cos(((2*arma::datum::pi)/(full_periods(h)))*t_ij);
+        B_pred.col(2*h+1) = cos(((2*arma::datum::pi)/(full_periods(h)))*tg);
     }
-    Lambdafile.close();
-    std::vector<std::vector<double>> thetatilde;
-    std::ifstream Thetatildefile("Thetatilde.csv");
-    while (std::getline(Thetatildefile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
-        }
-        thetatilde.push_back(row); // Aggiungi la riga alla matrice
-    }
-    Thetatildefile.close();
-    std::vector<std::vector<double>> theta;
-    std::ifstream THETAfile("THETA.csv");
-    while (std::getline(THETAfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
-        }
-        theta.push_back(row); // Aggiungi la riga alla matrice
-    }
-    THETAfile.close();
-    std::vector<std::vector<double>> W;
-    std::ifstream Wfile("W.csv");
-    while (std::getline(Wfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
-        }
-        W.push_back(row); // Aggiungi la riga alla matrice
-    }
-    Wfile.close();
-    std::vector<double> numeff;
-    std::ifstream numefffile("numeff.csv");
-    if (std::getline(Lambdafile, line)) {
-        std::stringstream ss(line); // Usa stringstream per analizzare la riga
-        std::string value;
-        while (std::getline(ss, value, ',')) {
-            lambda.push_back(std::stoi(value)); // Converti la stringa in int e aggiungi al vettore
+
+    // Define global constants
+    int rep = 1;
+    int nrun = 10000;                   // Number of iteration
+    int burn = 1000;                    // Burn-in
+    int thin = 5;                       // Thinning
+    double sp = (nrun - burn) / thin;   // Number of posterior samples
+    int k = 5;                          // Number of factors to start with (for now)
+    // int k = arma::floor(log(p)*4);   // Number of factors to start with (number of columns of Lambda)
+
+    double b0 = 1., b1 = 0.0005;         // Parameters for exponential - assumed to be arbitrary
+    double epsilon = 1e-3;              // Threshold limit
+    double prop = 1.00;                 // Proportion of redundant elements within columns
+
+    // Define hyper-parameter values
+    double as = 1., bs = 0.5;            // Gamma hyper-parameters for residual precision (true value res variance = 1 for every i)
+    double df = 3.;                      // Gamma hyper-parameters for t_{ij} [for phi_{ij} (i.e. rho)?]
+    double ad1 = 2.1, bd1 = 1.;          // Gamma hyper-parameters for delta_1
+    double ad2 = 3.1, bd2 = 1.;          // Gamma hyper-parameters delta_h, h >= 2
+    double adf = 1., bdf = 1.;            // Gamma hyper-parameters for ad1 and ad2 or df
+
+    // Initial values
+
+    // scalar_type s = randg<scalar_type>( distr_param(a,b) ), where scalar_type is either float or double
+    // a: shape parameter, b: scale parameter
+    arma::colvec sig = arma::randg(p, arma::distr_param(as, 1/bs));
+
+    arma::rowvec odd = arma::linspace<arma::rowvec>(1, 2*q - 1, q);
+    arma::rowvec even = arma::linspace<arma::rowvec>(2, 2*q, q);
+
+    arma::mat lambda(p, k, arma::fill::zeros);              // Factor loadings
+
+    arma::mat eta = arma::mvnrnd(arma::colvec(k, arma::fill::zeros),
+                                 arma::eye(k, k), T).t();   // Latent factors
+
+    arma::mat W = arma::mvnrnd(arma::colvec(k, arma::fill::zeros),
+                               arma::eye(k, k), 2*q);
+
+    arma::mat theta_tilde = lambda * W;                // Matrix of un-shrunk coefficients
+    arma::mat theta(p, 2*q, arma::fill::zeros);        // Matrix of (fixed) basis functions coefficients
+
+    double kappa_theta = 5.;                                                       // Upper bound on the Uniform prior on the thresholds
+    arma::mat thresholds = arma::randu(p, q, arma::distr_param(0., kappa_theta));  // Matrix of thresholds for theta
+
+    for (size_t i = 0; i < q; ++i)
+    {
+        arma::uvec index = arma::find(arma::norm(theta_tilde.cols(2*i, 2*i+1), 2) >= thresholds.col(i));
+        for(const auto& ind : index)
+        {
+            theta(ind, 2*i) = theta_tilde(ind, 2*i);
+            theta(ind, 2*i + 1) = theta_tilde(ind, 2*i + 1);
         }
     }
-    numefffile.close();
-    std::vector<std::vector<double>> Bpred;
-    std::ifstream Bpredfile("Bpred.csv");
-    while (std::getline(Bpredfile, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
+
+    arma::mat phi_ih = arma::randg(p, k, arma::distr_param(df/2, 2/df));
+    arma::vec delta(k, arma::fill::zeros);
+    delta.row(0) = arma::randg(arma::distr_param(ad1,bd1));
+    delta.rows(1,k-1) = arma::randg(k-1, 1, arma::distr_param(ad2,bd2));
+    arma::vec tau_h = arma::cumprod(delta);
+
+    //arma::vec v = arma::linspace(1, 4, 4);
+    //arma::mat test_mat = arma::repmat(v, 1, 4).t(); // TODO: just for testing
+    //v.print();
+    //test_mat.print();
+
+    arma::mat P_lam(p,k);
+    for (size_t i=0; i < p; ++i) {
+        P_lam.row(i) = phi_ih.row(i) % tau_h.t();
+    }
+
+    // This matrix stores the rejection probabilities 1 - mean(Accept) is the acceptance prob of the MH steps
+    arma::mat Acc2(nrun, p, arma::fill::zeros);
+
+    /// GIBBS SAMPLER!!!!
+
+    for (size_t i=0; i < 1; ++i) {
+
+        // Update error precisions
+        arma::mat Y_til = Y - B*theta.t() - eta*lambda.t();
+        double a = as + 0.5 * T;
+        arma::rowvec bs_vec(1,p, arma::fill::value(bs));
+        arma::rowvec ones_vec(1,p, arma::fill::value(1));
+        arma::mat b = ones_vec/(bs_vec + 0.5* arma::sum(arma::pow(Y_til,2)));
+        arma::vec sig(p);
+        for (size_t i = 0; i < p; ++i) {
+            sig(i) = arma::randg(arma::distr_param(a,b(i)));
+
         }
-        Bpred.push_back(row); // Aggiungi la riga alla matrice
-    }
-    Bpredfile.close();
-    std::vector<std::vector<double>> thr1;
-    std::ifstream thr1file("thr1.csv");
-    while (std::getline(thr1file, line)) {
-        std::stringstream ss(line);
-        std::string value;
-        std::vector<double> row; // Riga della matrice
-        while (std::getline(ss, value, ',')) {
-            row.push_back(std::stod(value)); // Converte la stringa in double
+
+        // Update eta
+        arma::mat lmsg(p,k);
+        std::cout << arma::size(lambda) << std::endl;
+        for (size_t i=0; i < k; ++i) {
+            lmsg.col(i) = lambda.col(i) % sig;
         }
-        thr1.push_back(row); // Aggiungi la riga alla matrice
-    }
-    thr1file.close();
-    int const q=5;
-    // dati se li carico direttamente da codice
-    //const int p= t[0].size()
-    //const int t=t.size()
-    //std::vector<double> tij, tg;
-    //    for (double t = 0; t <= 46; t += 2) {
-    //        tij.push_back(t / 46);
-    //    }
-    //    for (double t = 0; t <= 46; t += 0.1) {
-    //        tg.push_back(t / 46);
-    //    }
-    //    std::vector<std::vector<double>> B(T, std::vector<double>(2 * q, 0.0));
-    //    std::vector<std::vector<double>> Bpred(tg.size(), std::vector<double>(2 * q, 0.0));
-    //    std::vector<double> lambda2 = {8, 12, 16, 24, 48};
-    //    std::vector<double> periods(lambda2.size());
-    //    std::vector<double> lambda(lambda2.size());
-    //    for (size_t i = 0; i < lambda2.size(); ++i) {
-    //        periods[i] = lambda2[i] / 2;
-    //        lambda[i] = periods[i] / 46;
-    //    }
+        arma::mat veta_1 = arma::mat(k,k, arma::fill::eye) + lmsg.t() * lambda;
+        arma::mat t_chol = chol_cov(veta_1);
+        veta_1.brief_print();
+        arma::mat Q,R;
+        arma::qr(Q,R,t_chol);
+        arma::mat S=arma::inv(R);
+        arma::mat veta=S*S.t();
+        arma::mat Meta=(((Y-B*veta).t())*lmsg)*veta;
+        arma::mat eta=Meta+arma::randn(T,k)*S.t();
+        // Update of Lambda (rue & held)
+        for (size_t h=0; h < p; ++h) {
+                      // This is the cov matrix as in the paper; REMOVED: Z*eye(Ttilde)*Z'+
 
 
-    for(size_t h=0;h<lambda.size();h++){
-        double lambda_h = lambda[h];
-        double factor = 2 * M_PI / lambda_h;
-        for (size_t i = 0; i < T; ++i) {
-            B[i][2 * h] = std::sin(factor * tij[i]);
-            Bpred[i][2 * h] = std::sin(factor * tg[i]);
-
-            B[i][2 * h + 1] = std::cos(factor * tij[i]);
-            Bpred[i][2 * h + 1] = std::cos(factor * tg[i]);
         }
-    }
-    const size_t ktr = 20;
-    const size_t rep = 1;
-    const size_t nrun = 50;
-    const size_t burn = 20;
-    const size_t thin = 5;
-    const size_t sp = (nrun - burn)/thin; //Number of posterior samples
-    const size_t k = 5;  // Number of factors to start with (for now)
 
-    const size_t b0 = 1;
-    const size_t b1 = 0.0005;
-    const size_t epsilon = 1e-3; // Threshold limit
-    const size_t prop = 1.00; //Proportion of redundant elements within columns
-    // Define hyperparameter values
-    const size_t as = 1;
-    const size_t bs = 0.5;                           // Gamma hyperparameters for residual precision (true value res variance = 1 for every i)
-    const size_t df = 3;                                     // Gamma hyperparameters for t_{ij} ?? dubbio non sarebbe di phi
-    const size_t ad1 = 2.1;
-    const size_t bd1 = 1;                         // Gamma hyperparameters for delta_1
-    const size_t ad2 = 3.1;
-    const size_t bd2 = 1;                 // gamma hyperparameters delta_h, h >= 2
-    const size_t adf = 1;
-    const size_t bdf = 1;                           // Gamma hyperparameters for ad1 and ad2 or df
 
-//initial values
-    std::gamma_distribution<double> gamma_dist(as, 1.0 / bs);
-    std::vector<double> sig(p);
-    for (int i = 0; i < p; ++i) {
-        sig[i] = gamma_dist(generator);
-    }
-    std::vector<int> odd;
-    std::vector<int> even;
-    for (size_t i=1;i<=q;i++){
-        odd.push_back(2*i-1);
-        even.push_back(2*i);
-    }
+
+        }
+
+
+
+
+    return 0;
 }
 
+arma::mat chol_cov (const arma::mat& mat) {
+    arma::mat T;
+    if (mat.is_sympd()) {
+        T = arma::chol(mat, "upper");
+        T.brief_print();
+    }
+    else {
+        arma::vec eigvals;
+        arma::mat eigvecs;
+        // Calcola gli autovalori e gli autovettori
+        arma::eig_sym(eigvals, eigvecs, mat);
+        // Costruisci la matrice diagonale Lambda dagli autovalori
+        arma::mat Lambda = arma::diagmat(eigvals);
+        // Verifica la decomposizione A = V * Lambda * V^T
+        arma::mat T = eigvecs * Lambda * eigvecs.t();
+    }
+    return T;
+}
 
+void load_matrix (const std::string& file_path, const std::string& file_name, arma::mat &mat, bool print=true) {
+    std::string specific_path = file_path + file_name;
+    mat.load(specific_path, arma::csv_ascii);
+    if (print == true)
+        mat.print();
+}
+
+void load_vector (const std::string& file_path, const std::string& file_name, arma::vec &vec, bool print=true) {
+    std::string specific_path = file_path + file_name;
+    vec.load(specific_path, arma::csv_ascii);
+    if (print == true)
+        vec.raw_print();
+}
+
+void load_constant (const std::string& file_path, const std::string& file_name, int num, bool print=true) {
+    std::string specific_path = file_path + file_name;
+    std::ifstream num_file(specific_path);
+    num_file >> num;
+    if (print == true)
+        std::cout << num << std::endl;
+}
